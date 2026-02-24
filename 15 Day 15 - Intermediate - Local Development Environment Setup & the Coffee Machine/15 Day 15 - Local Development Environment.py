8442ba9cf38eeb16bcd358e926538de006085f06
Adil Nawaz <smadilnawaz.1122@gmail.com>
2026-02-24 21:42:42 +0500

    day_15 committ

---
+++ 15 Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine/15 Day 15 - Local Development Environment.py
@@ -0,0 +1,7 @@
+
+#Coffee machine program
+#Makes 3 hot flavors
+#1. Espresso 2. Lattee 3. Cappuccino
+50ml water      200ml water     250ml water
+18g coffee       24g coffee     24g coffee
+                150ml milk      100ml milk
